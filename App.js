168692b1d50b63c6463ba1aa5bcbf9a902339e0f
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2020-11-27 14:32:45 +0500

    :tada: Added text fields in the app

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Platform, Text, View } from "react-native";
+import { StyleSheet, Platform, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Starting to work on Discount Calculator App</Text>
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{fontSize:30,fontFamily:'Times New Roman'}}>Discount Calculator App</Text>
+      </View>
+      <View style={{flex:3,justifyContent:'center',alignItems:'center'}}>
+        <TextInput defaultValue={0} style={{fontSize:20}} placeholder="Original Price:" keyboardType="numeric" /> 
+        <TextInput defaultValue={0} style={{fontSize:20}} placeholder="Discount Percentage:"  keyboardType="numeric" maxLength={3}/> 
+      </View>
     </View>
   );
 }
